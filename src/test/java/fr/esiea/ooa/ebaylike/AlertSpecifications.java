3795072949d1368d946ac60bdfb9821d080460a6
Nic0w <nico.broquet@gmail.com>
2013-05-13 00:43:21 +0200

    Writing tests on the alert system.

--- src/test/java/fr/esiea/ooa/ebaylike/AlertSpecifications.java
+++ src/test/java/fr/esiea/ooa/ebaylike/AlertSpecifications.java
@@ -4,17 +4,27 @@
 package fr.esiea.ooa.ebaylike;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.util.Date;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import fr.esiea.ooa.ebaylike.api.Bid;
 import fr.esiea.ooa.ebaylike.api.Buyer;
 import fr.esiea.ooa.ebaylike.api.DateHelper;
+import fr.esiea.ooa.ebaylike.api.Offer;
 import fr.esiea.ooa.ebaylike.api.Product;
 import fr.esiea.ooa.ebaylike.api.Seller;
 import fr.esiea.ooa.ebaylike.api.exception.UserAlreadyExistsException;
+import fr.esiea.ooa.ebaylike.api.persistence.PersistenceAgent;
 import fr.esiea.ooa.ebaylike.api.persistence.StorageException;
+import fr.esiea.ooa.ebaylike.default_impl.DefaultAlertFactory;
+import fr.esiea.ooa.ebaylike.default_impl.DefaultBidFactory;
+import fr.esiea.ooa.ebaylike.default_impl.DefaultOffer;
+import fr.esiea.ooa.ebaylike.default_impl.p2.CollectionsDatabase;
 
 /**
  * @author nic0w
@@ -22,20 +32,71 @@ import fr.esiea.ooa.ebaylike.api.persistence.StorageException;
  */
 public class AlertSpecifications {
 
+	static final Logger asLogger = LoggerFactory.getLogger(AlertSpecifications.class);
+	
+	/**
+	 * Test if the seller automatically retrieve an alert when an offer is added to one of his bids.
+	 * 
+	 * @throws UserAlreadyExistsException
+	 * @throws StorageException
+	 */
 	@Test
 	public void testAutomaticAlertOnSeller() throws UserAlreadyExistsException, StorageException {
 		
-		Date limit = DateHelper.getTomorrowSameHour();
-		
 		BidPlatform platform = BidPlatform.getDefaultInstance(true);
 
-		Seller seller = platform.newUser("aRandomLogin", "Benjamin", "Franklin");
-		
-		Buyer buyer  = platform.newUser("aRandomLogin2", "Benjamin2", "Franklin2");
-		Buyer buyer2  = platform.newUser("aRandomLogin3", "Benjamin3", "Franklin3");
-
+		Date limit = DateHelper.getTomorrowSameHour();
 		Product p = platform.newProduct("test");
 		
+		
+		FakeSeller fakeSeller = new FakeSeller();
+		
+		
+		Buyer buyer  = platform.newUser("aRandomLogin2", "Benjamin2", "Franklin2");
+
+		CollectionsDatabase storage = new CollectionsDatabase();
+		storage.bind(DefaultOffer.class, Offer.class);
+		
+		DefaultBidFactory bidFactory = new DefaultBidFactory(storage, new DefaultAlertFactory());
+		
+		Bid bid =  bidFactory.createBid(fakeSeller, p, limit).publishIt(fakeSeller);
+		
+		buyer.bid(bid, 5);
+		
+		Offer lastOffer = storage.get(Offer.class).where(Bid.class).isEqualTo(bid).firstRow();
+		
+		assertThat(fakeSeller.receivedBid, is(bid));
+		assertThat(fakeSeller.receivedOffer, is(lastOffer));
 	}
+	
+	/*@Test
+	public void test*/
 
 }
+
+
+
+class FakeSeller implements Seller {
+
+	public Bid receivedBid;
+	public Offer receivedOffer;
+	
+	@Override
+	public void receivedNewOffer(Bid bid, Offer newOffer) {
+		
+		AlertSpecifications.asLogger.info("Hello from FakeSeller.receivedNewOffer() !");
+		
+		this.receivedBid = bid;
+		this.receivedOffer = newOffer;
+	}
+
+	@Override
+	public Bid createBid(Product p, Date limit) { return null; }
+
+	@Override
+	public Bid createBid(Product p, Date limit, float minPrice) { return null; }
+
+	@Override
+	public void publishBid(Bid bid) {}
+}
+
